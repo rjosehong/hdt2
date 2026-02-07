2d10279dba3eb43982c94336c0a74ef6d346a42a
rodrigo <rodrigojosehong@gmail.com>
2026-02-06 20:05:59 -0600

    Add files via upload

--- ProcesoAdt.java
+++ ProcesoAdt.java
@@ -2,9 +2,35 @@ public class ProcesoAdt{
     private Stack<Integer> stack;
     private Calculator calculator;
 
-    
+    public ProcesoAdt(Stack<Integer> stack, Calculator calculator) {
+        this.stack = stack;
+        this.calculator = calculator;
+    }
 
-    public static void main(String[] args) {
-        
+    public int evaluate(String expression) {
+        String[] tokens = expression.split("\\s+");
+
+        for (String token : tokens) {
+            if (token.matches("-?\\d+")) {
+                stack.push(Integer.parseInt(token));
+            } else {
+                int b = stack.pop();
+                int a = stack.pop();
+                int result = apply(token, a, b);
+                stack.push(result);
+            }
+        }
+        return stack.pop();
+    }
+
+    private int apply(String op, int a, int b) {
+        switch (op) {
+            case "+": return calculator.sumar(a, b);
+            case "-": return calculator.restar(a, b);
+            case "*": return calculator.multiplicar(a, b);
+            case "/": return calculator.dividir(a, b);
+            default:
+                throw new IllegalArgumentException("Invalid operator");
+        }
     }
 }
